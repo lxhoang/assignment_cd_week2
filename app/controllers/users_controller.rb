7bb9fd8557a346f0ef6037004dd85e4c49681764
Hoang Le <xh.dhhv@gmail.com>
2017-02-25 22:21:01 +0700

    Creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
+	
 	def new
-		@users = User.all
-
+		@user = User.new(email: params[:email])
 	end
 
+	def create
+		@user = User.new user_params
+		if @user.save
+			flash[:success] = "Welcome to site"
+			redirect_to root_path
+		else
+			render 'new'		
+		end
+	end
 
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :name, :password)
+	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 	has_secure_password
 
-	validates :email, uniqueness: true
+	validates :email, uniqueness: true, presence: true
 end
